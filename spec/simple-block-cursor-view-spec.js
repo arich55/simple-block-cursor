c6800177638f92b0b80504a7d48b197905be234a
Andre Richards <andrev.richards@gmail.com>
2018-10-30 17:35:58 -0400

    initial commit

---
+++ spec/simple-block-cursor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SimpleBlockCursorView from '../lib/simple-block-cursor-view';
+
+describe('SimpleBlockCursorView', () => {
+  it('has one valid test', () => {
+    expect('easy').toBe('easy');
+  });
+});
